4a54ba817f8971820cdaa6d9e8c9630360fefa65
Taras Kostyuk <taras.kostyuk@holmusk.com>
2025-01-07 14:54:55 +0100

    gopls, pyright, terraformls LSP servers

--- init.lua
+++ init.lua
@@ -617,8 +617,9 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
+        gopls = {}, -- after `brew install gopls`
+        pyright = {}, -- after `brew install pyright`
+        terraformls = {}, -- after `brew install terraformls`
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
